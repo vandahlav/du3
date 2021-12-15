cc9c1176501819df9953d4e938a8a59ac31e0ea6
Vanda Hlaváčková <hlavackva@natur.cuni.cz>
2021-12-15 16:11:34 +0100

    nejdelší vzdálenost ke kontejneru

--- du3.py
+++ du3.py
@@ -13,6 +13,7 @@ with open("adresy.geojson", encoding="utf-8") as adresy_2, open("kontejnery.geoj
     nejblizsi_vzdalenost = float('inf')
     pocet_adres = 0
     suma_vzdalenosti = 0
+    nejdelsi_vzdalenost = 0
     max_vzdalenost = 10000
     min_vzdalenosti_list = []
     
@@ -28,30 +29,36 @@ with open("adresy.geojson", encoding="utf-8") as adresy_2, open("kontejnery.geoj
         y_a = feature["geometry"]["coordinates"][1]
 
         #vytvoření nového klíče a uložení do slovníku 
-        feature ["geometry"]["coordinates"] = prevod_souradnic(x_a,y_a)
+        feature ["geometry"]["coordinates_prevod"] = prevod_souradnic(x_a,y_a)
         x_a,y_a = prevod_souradnic(x_a,y_a)
         adresa_ulice = feature ["properties"]["addr:street"]
         adresa_cp = feature ["properties"]["addr:housenumber"]
 
         #výpočet nejkratší vzdálenosti z dané adresy ke kontejneru 
-        seznam_vzdalenosti = []
         for feature in kontejnery_list:
             x_k, y_k = feature["geometry"]["coordinates"]
             vzdalenost = int(sqrt((x_k - x_a)**2 + (y_k - y_a)**2))
-            seznam_vzdalenosti.append(vzdalenost)
-
-        min_vzdalenosti_list.append(min(seznam_vzdalenosti)) 
-     
-        """if nejblizsi_vzdalenost > max_vzdalenost:
-            print(f"Nejbližší vzdálenost ke kontejneru z adresy {adresa_ulice} {adresa_cp} je větší než 10 km. Program končí.")
+            #seznam_vzdalenosti.append(vzdalenost)
+            if vzdalenost < nejblizsi_vzdalenost:
+                nejblizsi_vzdalenost = vzdalenost
+        pocet_adres += 1
+        suma_vzdalenosti += nejblizsi_vzdalenost
+    
+        #ošetření, aby vzdálenost ke kontejneru nebyla delší, než 10 km
+        if nejblizsi_vzdalenost > max_vzdalenost:
             adresa_ulice = feature ["properties"]["addr:street"]
             adresa_cp = feature ["properties"]["addr:housenumber"]
-            quit()"""
-
-prumerna_vzdalenost = mean(min_vzdalenosti_list)
+            print(f"Nejbližší vzdálenost ke kontejneru z adresy {adresa_ulice} {adresa_cp} je větší než 10 km. Program končí.")
+            quit()
+        
+        #zjištění adresy, která to má nejdále ke kontejneru
+        if nejblizsi_vzdalenost > nejdelsi_vzdalenost:
+            nejdelsi_vzdalenost = nejblizsi_vzdalenost
+            adresa_ulice_max = feature ["properties"]["addr:street"]
+            adresa_cp_max = feature ["properties"]["addr:housenumber"]
+            
+    prumerna_vzdalenost = suma_vzdalenosti / pocet_adres
 
 print(f"Průměrná vzdálenost ke kontejneru je {round(prumerna_vzdalenost)} m.")
-print(f"Nejdále to je ke kontejneru z adresy {adresa_ulice} {adresa_cp}.")  #nefunguje
-
-
+print(f"Nejdále to je ke kontejneru z adresy {adresa_ulice_max} {adresa_cp_max} a to {nejdelsi_vzdalenost} m.")
 
