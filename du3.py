5315184dd57476514beae67da7490b1adaaee982
Vanda Hlaváčková <hlavackva@natur.cuni.cz>
2021-12-15 22:03:21 +0100

    přidání fce na načtení souborů

--- du3.py
+++ du3.py
@@ -1,70 +1,68 @@
 from pyproj import Transformer
 import json   
-from funkce import prevod_souradnic
+from funkce import prevod_souradnic, otevreni_souboru
 from math import sqrt
 from statistics import mean, median
 
 #načtení souborů
-with open("adresy.geojson", encoding="utf-8") as adresy_2, open("kontejnery.geojson", encoding="utf-8") as kontejnery_2:
-    adresy = json.load(adresy_2)
-    kontejnery = json.load(kontejnery_2)  
+adresy = otevreni_souboru("adresy.geojson")
+kontejnery = otevreni_souboru("kontejnery.geojson") 
 
-    #inicializace proměnných 
-    nejblizsi_vzdalenost = float('inf')
-    pocet_adres = 0
-    suma_vzdalenosti = 0
-    nejdelsi_vzdalenost = 0
-    max_vzdalenost = 10000
-    min_vzdalenosti_list = []
-    
-    #vyselektování jen volných (=veřejných) kontejnerů
-    kontejnery_list = []
-    for feature in kontejnery["features"]:
-        if feature ["properties"]["PRISTUP"] == "volně":
-            kontejnery_list.append(feature)    
+#inicializace proměnných 
+nejblizsi_vzdalenost = float('inf')
+pocet_adres = 0
+suma_vzdalenosti = 0
+nejdelsi_vzdalenost = 0
+max_vzdalenost = 10000
+min_vzdalenosti_list = []
 
-    #načtení a převod adres 
-    for feature in adresy ["features"]:
-        x_a = feature["geometry"]["coordinates"][0]
-        y_a = feature["geometry"]["coordinates"][1]
+#vyselektování jen volných (=veřejných) kontejnerů
+kontejnery_list = []
+for feature in kontejnery["features"]:
+    if feature ["properties"]["PRISTUP"] == "volně":
+        kontejnery_list.append(feature)    
 
-        #vytvoření nového klíče a uložení do slovníku 
-        feature ["geometry"]["coordinates_prevod"] = prevod_souradnic(x_a,y_a)
-        x_a,y_a = prevod_souradnic(x_a,y_a)
+#načtení a převod adres 
+for feature in adresy ["features"]:
+    x_a = feature["geometry"]["coordinates"][0]
+    y_a = feature["geometry"]["coordinates"][1]
+
+    #vytvoření nového klíče a uložení do slovníku 
+    feature ["geometry"]["coordinates_prevod"] = prevod_souradnic(x_a,y_a)
+    x_a,y_a = prevod_souradnic(x_a,y_a)
+    adresa_ulice = feature ["properties"]["addr:street"]
+    adresa_cp = feature ["properties"]["addr:housenumber"]
+
+    #výpočet nejkratší vzdálenosti z dané adresy ke kontejneru 
+    for feature in kontejnery_list:
+        x_k, y_k = feature["geometry"]["coordinates"]
+        vzdalenost = int(sqrt((x_k - x_a)**2 + (y_k - y_a)**2))
+        #seznam_vzdalenosti.append(vzdalenost)
+        if vzdalenost < nejblizsi_vzdalenost:
+            nejblizsi_vzdalenost = vzdalenost
+    pocet_adres += 1
+    suma_vzdalenosti += nejblizsi_vzdalenost
+    min_vzdalenosti_list.append(nejblizsi_vzdalenost)
+
+    #ošetření, aby vzdálenost ke kontejneru nebyla delší, než 10 km
+    if nejblizsi_vzdalenost > max_vzdalenost:
         adresa_ulice = feature ["properties"]["addr:street"]
         adresa_cp = feature ["properties"]["addr:housenumber"]
-
-        #výpočet nejkratší vzdálenosti z dané adresy ke kontejneru 
-        for feature in kontejnery_list:
-            x_k, y_k = feature["geometry"]["coordinates"]
-            vzdalenost = int(sqrt((x_k - x_a)**2 + (y_k - y_a)**2))
-            #seznam_vzdalenosti.append(vzdalenost)
-            if vzdalenost < nejblizsi_vzdalenost:
-                nejblizsi_vzdalenost = vzdalenost
-        pocet_adres += 1
-        suma_vzdalenosti += nejblizsi_vzdalenost
-        min_vzdalenosti_list.append(nejblizsi_vzdalenost)
+        print(f"Nejbližší vzdálenost ke kontejneru z adresy {adresa_ulice} {adresa_cp} je větší než 10 km. Program končí.")
+        quit()
     
-        #ošetření, aby vzdálenost ke kontejneru nebyla delší, než 10 km
-        if nejblizsi_vzdalenost > max_vzdalenost:
-            adresa_ulice = feature ["properties"]["addr:street"]
-            adresa_cp = feature ["properties"]["addr:housenumber"]
-            print(f"Nejbližší vzdálenost ke kontejneru z adresy {adresa_ulice} {adresa_cp} je větší než 10 km. Program končí.")
-            quit()
+    #zjištění adresy, která to má nejdále ke kontejneru
+    if nejblizsi_vzdalenost > nejdelsi_vzdalenost:
+        nejdelsi_vzdalenost = nejblizsi_vzdalenost
+        adresa_ulice_max = feature ["properties"]["addr:street"]
+        adresa_cp_max = feature ["properties"]["addr:housenumber"]
+    
+    #výpočet mediánu
+    m = median(min_vzdalenosti_list)
         
-        #zjištění adresy, která to má nejdále ke kontejneru
-        if nejblizsi_vzdalenost > nejdelsi_vzdalenost:
-            nejdelsi_vzdalenost = nejblizsi_vzdalenost
-            adresa_ulice_max = feature ["properties"]["addr:street"]
-            adresa_cp_max = feature ["properties"]["addr:housenumber"]
+prumerna_vzdalenost = suma_vzdalenosti / pocet_adres
 
-        #výpočet mediánu
-        m = median(min_vzdalenosti_list)
-            
-    prumerna_vzdalenost = suma_vzdalenosti / pocet_adres
-    
 
 print(f"Průměrná vzdálenost ke kontejneru je {round(prumerna_vzdalenost)} m.")
 print(f"Nejdále to je ke kontejneru z adresy {adresa_ulice_max} {adresa_cp_max} a to {nejdelsi_vzdalenost} m.")
 print(f"Medián vzdáleností ke kontejnerů je {round(m)}.")
-
--- funkce.py
+++ funkce.py
@@ -1,6 +1,16 @@
 from pyproj import Transformer
+import json
 
 def prevod_souradnic(x,y):
     wgs2jtsk = Transformer.from_crs(4326,5514, always_xy=True)
     souradnice = wgs2jtsk.transform(x,y)
     return souradnice
+
+def otevreni_souboru(soubor):
+    try:
+        with open(soubor, encoding="utf-8") as soubor:
+            return json.load(soubor)
+    except FileNotFoundError:
+        print("Vstupní soubor se nepodařilo načíst. Ujistěte se, že daný soubor existuje, případně zda je k němu zadána korektní cesta.")
+    except PermissionError:
+        print("Program nemá přístup k zápisu výstupních souborů.")
