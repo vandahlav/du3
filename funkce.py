ae10297db7b142770a3bd82103e831331f0bcc9e
Vanda Hlaváčková <hlavackva@natur.cuni.cz>
2021-12-15 22:27:59 +0100

    přidání quit() do try blocku

--- funkce.py
+++ funkce.py
@@ -12,5 +12,7 @@ def otevreni_souboru(soubor):
             return json.load(soubor)
     except FileNotFoundError:
         print("Vstupní soubor se nepodařilo načíst. Ujistěte se, že daný soubor existuje, případně zda je k němu zadána korektní cesta.")
+        quit()
     except PermissionError:
         print("Program nemá přístup k zápisu výstupních souborů.")
+        quit()
